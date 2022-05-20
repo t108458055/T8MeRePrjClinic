92c095d993e0984ce4419d460049480aac2c9a8f
enzhong <62697271+XiaoEnZhong@users.noreply.github.com>
2022-05-20 23:22:08 +0800

    CRUD
    CRUD

--- RePrjClinicAppoint/RePrjClinicAppoint/Controllers/UserController.cs
+++ RePrjClinicAppoint/RePrjClinicAppoint/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RePrjClinicAppoint.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,22 @@ namespace RePrjClinicAppoint.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+
+        public IActionResult pushUser(NewUserModel NewUser)
+        {
+            var YA = NewUser;
+            Console.WriteLine(YA);
+            return Content("OK");
+        }
+        public class NewUserModel
+        {
+            public string UserName { get; set; }
+            public string UserPassword { get; set; }
+            public string UserEmail { get; set; }
+        }
+
 
     }
 }
